76b980845685509f41e63871ec6f6aea62de5327
Erik Marsh <erik.i.marsh@gmail.com>
2025-03-20 01:56:36 -0700

    Disable warning in Clay header
    This warning only occurs when including clay.h without the
    implementation. Seems like some internal Clay logic depends on an
    unused variable.

--- Renderer.hpp
+++ Renderer.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+// Ignores a warning thrown by some clay internal workings that are irrelevant here
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
 #include <clay.h>
+#pragma GCC diagnostic pop
+
 #include <raylib.h>
 
 Clay_Dimensions MeasureText(Clay_StringSlice text, Clay_TextElementConfig *config, void *userData);
